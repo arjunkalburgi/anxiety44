989e2c51519ebb0cb2edc16eeec7280867305446
Arjun Kalburgi <askalburgi@gmail.com>
2017-05-24 23:57:06 -0600

    updatesss

--- wordpress/header.php
+++ wordpress/header.php
@@ -4,6 +4,7 @@
 	<title>anxiety44</title>
 	
 	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="https://cdn.rawgit.com/askalburgi/anxiety44/57c39e48/wordpress/style.css">
 
 
 	<script src="https://unpkg.com/scrollreveal/dist/scrollreveal.min.js"></script>
@@ -24,9 +25,9 @@
 		<div class="nav-wrapper">
 			<a href="#" class="brand-logo">A44</a>
 			<ul id="nav-mobile" class="right hide-on-med-and-down">
-				<li><a href="sass.html">HOME</a></li>
-				<li><a href="badges.html">CONTACT</a></li>
-				<li><a href="collapsible.html">IG</a></li>
+				<li><a href="http://anxiety44.bigcartel.com/">STORE</a></li>
+				<li><a href="http://www.anxiety44.com/contact">CONTACT</a></li>
+				<li><a href="https://www.instagram.com/anxiety_44">IG</a></li>
 			</ul>
 		</div>
 	</nav>
