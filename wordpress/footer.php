ced7cfc3afbd59b96559b7ed785522c9e0ce7d76
Arjun Kalburgi <askalburgi@gmail.com>
2017-06-24 19:02:39 -0600

    title hover

--- wordpress/footer.php
+++ wordpress/footer.php
@@ -5,21 +5,21 @@
 	</footer>
 
 	<script>
-		var blurElement = {blur:0};//start the blur at 0 pixels
+		// var blurElement = {blur:0};//start the blur at 0 pixels
 		$(function () { // wait for document ready
 			// init controller
 			var controller = new ScrollMagic.Controller();
 
 			// build tween
-			var tween = TweenMax.to(blurElement, 1, {blur:50, onUpdate:applyBlur, ease: Power0.easeInOut});
+			// var tween = TweenMax.to(blurElement, 1, {blur:50, onUpdate:applyBlur, ease: Power0.easeInOut});
 			// var tween = TweenMax.to("#logo", 1, {rotation: 360, ease: Linear.easeNone});
 
 			// build scene
-			var scene = new ScrollMagic.Scene({triggerElement: "#trigga", duration: 300})
-							.setTween(tween)
+			// var scene = new ScrollMagic.Scene({triggerElement: "#trigga", duration: 300})
+							// .setTween(tween)
 							// .setPin("a#logo", {pushFollowers: false})
 							// .addIndicators() // add indicators (requires plugin)
-							.addTo(controller);
+							// .addTo(controller);
 
 			var tween2 = TweenMax.to("nav", 1, {css:{opacity:1}, ease: Power4.easeInOut})
 			var scene2 = new ScrollMagic.Scene({triggerElement: "content"})
@@ -27,10 +27,10 @@
 							.addTo(controller); 
 		})
 
-		function applyBlur() {
+		// function applyBlur() {
 			// $('header a#logo').removeClass("fixed"); 
-		    TweenMax.set($('#logo'), {webkitFilter:"blur(" +blurElement.blur + "px)"});
-		};
+		    // TweenMax.set($('#logo'), {webkitFilter:"blur(" +blurElement.blur + "px)"});
+		// };
 	</script>
 
 	<script type="text/javascript">
